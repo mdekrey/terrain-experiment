65fdc4fb86051ddfeb495806daa3e4aab504ff6b
Matt DeKrey <mattdekrey@gmail.com>
2019-05-31 23:36:39 -0500

    Add treasure points

--- src/cave-generation/CaveGenerator.test.ts
+++ src/cave-generation/CaveGenerator.test.ts
@@ -1,10 +1,22 @@
 import { CaveGenerator } from "./CaveGenerator";
 
 it("generates some terrain", async () => {
-    const cave = new CaveGenerator(0, 50, 50, 2);
+  const cave = new CaveGenerator(0, 50, 50, 2);
 
-    const map = await cave.map;
-    const mapDisplay = map.map(row => row.map(e => e ? "█" : " ").join("")).join("\n");
+  const map = await cave.map;
+  const treasure = await cave.treasure;
+  const mapDisplay = map
+    .map((row, y) =>
+      row
+        .map((e, x) =>
+          treasure.find(entry =>
+            entry.x === x && entry.y === y) ? "*"
+            : e ? "█"
+            : " "
+        )
+        .join("")
+    )
+    .join("\n");
 
-    expect(mapDisplay).toMatchSnapshot();
+  expect(mapDisplay).toMatchSnapshot();
 });
--- src/cave-generation/CaveGenerator.ts
+++ src/cave-generation/CaveGenerator.ts
@@ -1,26 +1,31 @@
 import { Perlin } from "libnoise-ts/module/generator";
+import { GameCoordinates } from "../game/GameCoordinates";
 
 const deathLimit = 3;
 const birthLimit = 4;
+const treasureLimit = 5;
+
+interface Cave {
+    isSolid: boolean[][];
+    treasure: GameCoordinates[];
+}
 
 export class CaveGenerator {
     private readonly perlin = new Perlin();
-    private readonly result: Promise<boolean[][]>;
+    private readonly result: Promise<Cave>;
 
     constructor(seed: number, width: number, height: number, normalizationRounds: number) {
         this.perlin.seed = seed;
-        // this.perlin.frequency = 0.5;
         this.perlin.lacunarity = 3.4;
 
         const random = (x: number, y: number) => this.perlin.getValue(x, y, 0) < -0.05;
-        // const random = (x: number, y: number) => Math.random() < 0.45;
 
-        this.result = new Promise<boolean[][]>((resolve, reject) => {
+        this.result = new Promise<Cave>((resolve, reject) => {
             let map = Array.from(Array(height).keys()).map((_, y) => Array.from(Array(width).keys()).map((_, x) => random(x, y)));
             const v = map.reduce((prev, next) => [...prev, ...next], []);
             let count = 0;
 
-            function countAliveNeighbours(map: boolean[][], x: number, y: number){
+            function countAliveNeighbours(x: number, y: number){
                 let count = 0;
                 for(let i = -1; i < 2; i++){
                     for(let j = -1; j < 2; j++){
@@ -44,7 +49,7 @@ export class CaveGenerator {
 
             function doSimulationStep() {
                 map = map.map((_, y) => _.map((currentCell, x) => {
-                    const nbs = countAliveNeighbours(map, x, y);
+                    const nbs = countAliveNeighbours(x, y);
                     if (currentCell) {
                         return nbs >= deathLimit;
                     } else {
@@ -53,12 +58,20 @@ export class CaveGenerator {
                 }));
             }
 
+            function findTreasure() {
+                return map.map((_, y) => _.map((currentCell, x): (false | GameCoordinates) => {
+                    const nbs = countAliveNeighbours(x, y);
+                    return !currentCell && nbs >= treasureLimit ? { x, y } : false;
+                })).reduce((prev, next) => [...prev, ...next], []).filter((v): v is GameCoordinates => Boolean(v));
+            }
+
             function step() {
                 if (count++ < normalizationRounds) {
                     doSimulationStep();
                     setTimeout(step);
                 } else {
-                    resolve(map);
+
+                    resolve({ isSolid: map, treasure: findTreasure() });
                 }
             }
 
@@ -67,6 +80,10 @@ export class CaveGenerator {
     }
 
     get map() {
-        return this.result;
+        return this.result.then(cave => cave.isSolid);
+    }
+
+    get treasure() {
+        return this.result.then(cave => cave.treasure);
     }
 }
